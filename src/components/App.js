cbbe511666d164ea6434a194b15043d225598200
Nassim <aa.nassim.ddr@gmail.com>
2022-03-04 12:01:30 +0100

    finished signup

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import Main from './Main'
 
 function App() {
   return (
-    <Main cnx="TRUE" cur="SIGNIN"/>
+    <Main cnx="FALSE" cur="SIGNUP"/>
     );
 }
 
--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-class Signin extends React.Component {
-    constructor(props) {
-        super(props)
-    }
 
+
+
+class Signin extends React.Component {
     render() {
         return (
             <div className="App">
-            <h1>SIGNIN</h1>
+                <h1>SIGNIN</h1>
             </div>
       );
     }
